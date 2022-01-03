3312c49265284c99c3822e6b61b60e392fa6b106
Chris <chris@users.noreply.github.com>
2022-01-03 22:17:42 +0000

    mixed lists

---
+++ codechallenge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+user_input = input("please state your name:")
+user_input2 = input("please state the day of the week:")
+
+print("Hello, " + user_input + " Happy " + user_input2 + "!")
+
+
